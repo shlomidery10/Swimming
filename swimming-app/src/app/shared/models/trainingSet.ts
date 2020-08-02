542be9c469c04f70b8d33d69ae5fab20e133963a
shlomidery10 <shlomidery10@gmail.com>
2020-08-02 10:43:47 +0300

    /

--- swimming-app/src/app/shared/models/trainingSet.ts
+++ swimming-app/src/app/shared/models/trainingSet.ts
@@ -5,3 +5,43 @@ export class TrainingSet {
     workoutSet:WorkoutSet
     Parts_of_the_training:Array<WorkoutSet>;
   }
+
+
+
+  // export class Exercise {
+  //   constructor(repetition, distance,description,exerciseSets) {
+  //     this.repetition = repetition;
+  //     this.distance = distance;
+  //     this.description = description;
+  //     this.exerciseSets=exerciseSets;
+  //   }
+  // }
+
+
+  // export class ExerciseSet {
+  //   constructor(description, distance) {
+  //     this.description = description;
+  //     this.distance = distance;
+  //   }
+  // }
+
+
+  // export class Workout {
+  //   constructor(trainerName, week,goalOfTraining,date,WorkoutParts) {
+  //     this.trainerName = trainerName;
+  //     this.week = week;
+  //     this.goalOfTraining = goalOfTraining;
+  //     this.date = date;
+  //     this.WorkoutParts = WorkoutParts;
+  //   }
+  // }
+
+
+  // export class WorkoutPart {
+  //   constructor(name, goal,exercises) {
+  //     this.name = name;
+  //     this.goal = goal;
+  //     this.exercises = exercises;
+  //   }
+  // }
+
